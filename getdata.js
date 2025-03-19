066148cb6f053b08d9c2a1d193814e56f9e7ef15
Nitro-Gen Studios <nitrogen.studios.yt@gmail.com>
2025-03-19 09:38:16 +0100

    get data and render projects

---
+++ getdata.js
@@ -0,0 +1,13 @@
+async function GetData(url, render_func)
+{
+    try 
+    {
+        const response = await fetch(url)
+        const data = await response.json()
+        render_func(data)
+    } 
+    catch (error) 
+    {
+        console.log(error)
+    }
+}
--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 const url = "https://raw.githubusercontent.com/NitroGenStudios/PortfolioJSON/refs/heads/main/portfolio.json"
 // https://console.cloudinary.com/console/c-2049beeb7e2e950effd5dc0a224cbd/media_library/folders/caa5c3de4a89670b9be405a89abcd62200?view_mode=mosaic&sortField=publicId&sortDirection=asc
 
+function CreateProjectCard(data)
+{
+    var temakorok = ""
+
+    data.topics.forEach(temakor => {
+        temakorok += `<li>${temakor}</li>`
+    });
+
+    return `<div class="project-container">
+                <div class="project-layout">
+                    <div class="project-img">
+                        <img src="${data.photo_url}" alt="">
+                    </div>
+                    <div class="project-description">
+                        <h3>${data.title}</h3>
+                        <hr>
+                        <p>Témakörök</p>
+                        <ul>
+                            ${temakorok}
+                        </ul>
+
+                        <div class="project-links">
+                            <a href="${data.repo_link}" target="_blank" style="text-align: right;">Github link</a>
+                            <a href="${data.site_link}" target="_blank" style="text-align: right;">Weboldal</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function Render(json)
+{
+    const container = document.getElementById("projects")
+
+    json.forEach(data => {
+        container.innerHTML += CreateProjectCard(data)
+    });
+}
+
+GetData(url, Render)
